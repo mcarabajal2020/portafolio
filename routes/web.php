6f0ea19da8de7373ac6864fc011f1b3049fa4bb4
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2023-02-08 20:53:13 -0300

    Integracion de AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('portfolio');
 });
 
-Route::get('/portfolio', function () {
-    return view('portfolio');
+Route::get('/admin', function () {
+    return view('admin');
 });
