8178060c34d48d6e48b447ccfe9e42d8fe4d669a
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2023-04-22 20:14:39 -0300

    tarjetas portfolio

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -71,18 +71,25 @@
                             <h2 class="text-dark">PORTFOLIO</h2>
                             <hr>
                         </div>
-                        <div class="col-4 text-center mt-5">
-                            <h1>Coop. Electrica de Pasteur</h1>
-                           <a href="http://www.intercoopasteur.com.ar"> <img src="{{asset('https://intercoopasteur.com.ar/wp-content/uploads/2020/06/logo-de-la-Cooperativa-1024x960.png')}}" alt="Cooperativa Electrica de Pasteur" width="100"></a> 
+                        <div class="card m-auto text-center" style="width: 18rem;">
+                            <a href="http://www.intercoopasteur.com.ar"> <img src="{{asset('https://intercoopasteur.com.ar/wp-content/uploads/2020/06/logo-de-la-Cooperativa-1024x960.png')}}" class="mt-3" alt="Cooperativa Electrica de Pasteur" width="100"></a> 
+                            <div class="card-body">
+                                <p>Coop. Electrica de Pasteur</p>
+                            </div>
                         </div>
-                        <div class="col-4 text-center mt-5">
-                            <h1>Coop. De Agua Henderson</h1>
-                            <a href="http://www.coophenderson.com.ar"> <img src="{{asset('https://coophenderson.com.ar/wp-content/uploads/2021/11/Logo-Cooperativa-150x150.png')}}" alt="Cooperativa de Agua Potable y otros Servicios Publicos de Henderson LTDA" width="100"></a>
+                        <div class="card m-auto text-center" style="width: 18rem;">
+                            <a href="http://www.coophenderson.com.ar"> <img src="{{asset('https://coophenderson.com.ar/wp-content/uploads/2021/11/Logo-Cooperativa-150x150.png')}}" class="mt-3" alt="Cooperativa de Agua Potable y otros Servicios Publicos de Henderson LTDA" width="100"></a> 
+                            <div class="card-body">
+                                <p> De Agua Henderson</p>
+                            </div>
                         </div>
-                        <div class="col-4 text-center mt-5">
-                            <h1>El Inca - Consagro</h1>
-                            <a href="https://consultoragropecuario.com.ar"><img src="{{asset('https://consultoragropecuario.com.ar/wp-content/uploads/2022/05/Logo-elIn-png-768x559.png')}}" alt="El Inca - Consagro" width="100"></a>
+                        <div class="card m-auto text-center" style="width: 18rem;">
+                            <a href="https://consultoragropecuario.com.ar"> <img src="{{asset('https://consultoragropecuario.com.ar/wp-content/uploads/2022/05/Logo-elIn-png-768x559.png')}}" class="mt-3" alt="El Inca - Consagro" width="100"></a> 
+                            <div class="card-body">
+                                <p> Inca - Consagro </p>
+                            </div>
                         </div>
+                       
                     </div>
             </section>
 
