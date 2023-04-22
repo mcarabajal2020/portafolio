b2dfe9eb8cc111d813ddaed5ca31d33444798a63
mcarabajal2020 <alejandrom.carabajal@gmail.com>
2023-04-22 19:42:27 -0300

    formulario de contactos

--- resources/views/contact/form.blade.php
+++ resources/views/contact/form.blade.php
@@ -5,44 +5,48 @@
   
 @section('content')
 
-    <div class="d-flex justify-content-center align-items-center vh-100 " >
+    <div class="d-sm-flex justify-content-center align-items-center vh-100 " >
         <div class="w-33 shadow-lg p-3 mb-5 bg-body rounded">
             <h1>Contactame</h1>
             <p>Tenes alguna duda o te gustaria que trabajemos juntos en un proyecto?</p>
             <div class="">
                 <form action="{{ route('contact.send') }}" method="POST">
-                @csrf
+                    @csrf
                     <div class="form-group row mt-5">
-                        <label class="col-lg-2 col-form-label">Nombre</label>
+                        <label class="col-sm-2 col-form-label">Nombre</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control-plaintext  border border-dark rounded" name="name" placeholder="Ingrese su Nombre">
+                            <input type="text" class="form-control-plaintext  border border-dark rounded" name="name" placeholder=" Ingrese su Nombre">
                         </div>
                     </div>
                     <div class="form-group row mt-5">
                         <label class="col-sm-2 col-form-label">Telefono</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control-plaintext border border-dark rounded" name="phone" placeholder="Ingrese su Telefono">
+                            <input type="text" class="form-control-plaintext border border-dark rounded" name="phone" placeholder=" Ingrese su Telefono">
                         </div>
                     </div>
                     <div class="form-group row mt-5">
                         <label class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
-                            <input type="email" class="form-control-plaintext border border-dark rounded" name="email" placeholder="Ingrese su Email">
+                            <input type="email" class="form-control-plaintext border border-dark rounded" name="email" placeholder=" Ingrese su Email">
                         </div>
                     </div>
                     <div class="form-group row mt-5">
                         <label class="col-sm-2 col-form-label">Asunto</label>
                         <div class="col-sm-10">
-                        <input type="text" class="form-control-plaintext border border-dark rounded" name="subject" placeholder="Asunto">
+                        <input type="text" class="form-control-plaintext border border-dark rounded" name="subject" placeholder=" Asunto">
                         </div>
                     </div>
                     <div class="form-group row mt-5">
                         <label class="col-sm-2 col-form-label">Mensaje</label>
                         <div class="col-sm-10 border border-dark rounded">
-                            <textarea name="message" cols="50" placeholder="Ingrese su Consulta"></textarea>
+                            <textarea class="form-control" name="message" placeholder="Ingrese su Consulta"></textarea>
                         </div>
-                    <button class="btn btn-success col-md-13 mt-3">Enviar</button>
+                        <button class="btn btn-success col-md-13 mt-3">Enviar</button>
                     </div>
+                    <div class="align-items-center">
+                      
+                    </div>
+                    
                 </form>
             </div>
         </div>
